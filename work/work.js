b64b76b1db04529a7ec784a7619dc20e58715ee7
GIitchedGadget <90413092+GIitchedGadget@users.noreply.github.com>
2023-09-17 17:20:58 -0400

    yay

---
+++ work/work.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const discordElement = document.getElementById('discord');
+    if (!discordElement) {
+        console.error("Discord element not found!");
+        return;
+    }
+
+    discordElement.addEventListener('click', function() {
+
+        const text = document.getElementById('textToCopy').textContent;
+        navigator.clipboard.writeText(text).then(function() {
+
+            const tooltip = document.getElementById('tooltip');
+            tooltip.classList.add('visible');
+
+            setTimeout(function() {
+                tooltip.classList.remove('visible');
+            }, 2000);
+        }).catch(function(err) {
+            console.error('Unable to copy text: ', err);
+        });
+    });
+});
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+window.addEventListener('load', function() {
+    setTimeout(function() { //optional delay
+        document.body.classList.add('content-loaded');
+    }, 100);
+});
